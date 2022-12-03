5a6d40e52155203d7db8bfe21a885787f3b82ca8
Chaitanya Gadodia <chaitanya.gadodia01@gmail.com>
2022-12-03 12:14:22 +0530

    Add Habit (#2)

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { MoralisProvider } from "react-moralis";
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import HabitsList from './components/User';
+import AddHabit from './components/AddHabit';
 
 function App() {
   return (
     <MoralisProvider initializeOnMount={false}>
       <div className="App">
         <Header />
+        <div>For Individuals / Groups / Workspaces</div>
         {/* <SimpleStore /> */}
         <Dashboard />
         <HabitsList />
+        <AddHabit />
       </div>
     </MoralisProvider>
   );
